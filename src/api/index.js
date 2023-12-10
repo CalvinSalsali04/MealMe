b74bf0530cebaec80bc129a38aea1ff0d6ff1475
CalvinSalsali04 <c.salsali1@gmail.com>
2023-12-10 15:52:27 -0500

    weatherapi

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,6 @@ const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
 
 
-
-
-
 export const getPlacesData = async (sw, ne) => {
   try{
     const { data: { data}} = await axios.get(URL, {
@@ -27,6 +24,24 @@ export const getPlacesData = async (sw, ne) => {
     console.log(data);
     return data;
   } catch(error){
+    console.log(error);
+  }
+}
+
+
+export const getWeatherData = async (query) => {
+  try {
+    const { data } = await axios.get('https://weatherapi-com.p.rapidapi.com/current.json', {
+      params: { q: query },
+      headers: {
+        'X-RapidAPI-Key': '1feb754b00msh39403b508d9db3ap14009fjsnde32d0770579',
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+      }
+    });
+
+    console.log(data);
+    return data;
+  } catch (error) {
     console.log(error);
   }
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,44 @@ import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
 import useStyles from './styles';
+import { useState, useEffect } from 'react';
 
-const Header = () => {
+import { getWeatherData } from '../../api'; 
+
+
+const Header = ({setCoordinates}) => {
   const classes = useStyles();
+  const [autocomplete, setAutoComplete] = useState(null);
+  
+  const [weather, setWeather] = useState({});
+
+  const onLoad = (autoC) => setAutoComplete(autoC);
+
+  const onPlaceChanged = async () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+
+    setCoordinates({ lat, lng });  
+    
+    const weatherData = await getWeatherData(`${lat},${lng}`);
+    setWeather(weatherData);
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const { latitude, longitude } = position.coords;
+      
+      setCoordinates({ lat: latitude, lng: longitude });
+  
+      try {
+        const weatherData = await getWeatherData(`${latitude},${longitude}`);
+        setWeather(weatherData);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }, [])
+  
   
   return(
     <AppBar position='static'>
@@ -15,17 +50,24 @@ const Header = () => {
           MealMe
         </Typography>
         <Box display="flex">
+
+
+        <Typography variant='h6' className={classes.title}>
+            Search
+          </Typography>
+
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase placeholder='Search...' classes={{ root: classes.inputRoot, input: classes.inputInput}}/>
+              </div>
+          </Autocomplete>
           <Typography variant='h6' className={classes.title}>
-            Enter Area   
+            {weather.current ? `${weather.current.condition.text}, ${weather.current.temp_c}°C` : 'Search'}
           </Typography>
           
-          
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase placeholder='Search...' classes={{ root: classes.inputRoot, input: classes.inputInput}}/>
-            </div>
           
           
         </Box>
